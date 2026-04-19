449738b9c78619520d572ed0123107c175221322
j5vz <hamzagurcay832@gmail.com>
2026-04-20 01:26:51 +0300

    update

--- inject/main.js
+++ inject/main.js
@@ -11,25 +11,20 @@ let Main = {
     inverseCache: 0n,
 
     Inverse(a, n) {
-        // LOL STOLEN
         let t = 0n;
         let newt = 1n;
-
         let r = n;
         let newr = a;
 
         while (newr != 0n) {
             let q = r / newr;
-
             let curt = t;
             t = newt;
             newt = (curt - q * newt) & 0xFFFFFFFFFFFFFFFFn;
-
             let curr = r;
             r = newr;
             newr = (curr - q * newr) & 0xFFFFFFFFFFFFFFFFn;
         }
-
         return (t < 0n) ? (t + n) & 0xFFFFFFFFFFFFFFFFn : t;
     },
 
@@ -37,73 +32,56 @@ let Main = {
         for (let i = 0; i < proto.instructions.length; i++) {
             proto.instructions[i] = Number((BigInt(proto.instructions[i]) * Main.inverseCache) & 0xFFFFFFFFn);
         }
-
         for (let i = 0; i < proto.protos.length; i++) {
             proto.protos[i].source = proto.source;
-
             Main.EncodeInstructions(proto.protos[i]);
         }
     },
 
     ValidateScriptContext(sc) {
-        // maybe more?
-        return Memory.ReadU32(sc) === Offsets.SCRIPT_CONTEXT_VTABLE && Memory.ReadU32(sc + Offsets.INSTANCE_DESCRIPTOR) === Offsets.SCRIPT_CONTEXT_DESCRIPTOR;
+        return Memory.ReadU32(sc) === Offsets.SCRIPT_CONTEXT_VTABLE && 
+               Memory.ReadU32(sc + Offsets.INSTANCE_DESCRIPTOR) === Offsets.SCRIPT_CONTEXT_DESCRIPTOR;
     },
 
     FindOrGetScriptContext() {
-        if (Main.scriptContext)
-            return Main.scriptContext;
+        if (Main.scriptContext) return Main.scriptContext;
+        if (typeof GROWABLE_HEAP_U32 == "undefined") return 0;
 
-        if (typeof GROWABLE_HEAP_U32 == "undefined")
-            return 0;
-
-        for (let i = 0; i < GROWABLE_HEAP_U32().length; i++) {
-            if (GROWABLE_HEAP_U32()[i] === Offsets.SCRIPT_CONTEXT_VTABLE && Main.ValidateScriptContext(i << 2)) {
+        let heap = GROWABLE_HEAP_U32();
+        for (let i = 0; i < heap.length; i++) {
+            if (heap[i] === Offsets.SCRIPT_CONTEXT_VTABLE && Main.ValidateScriptContext(i << 2)) {
                 Main.scriptContext = i << 2;
-
                 break;
             }
         }
-
         return Main.scriptContext;
     },
 
-    // also gets ckey
     InitOrGetExploitState() {
-        if (Main.executorGlobalState)
-            return Main.executorGlobalState;
-
-        if (!Main.scriptContext)
-            return 0;
+        if (Main.executorGlobalState) return Main.executorGlobalState;
+        if (!Main.scriptContext) return 0;
 
         const GLOBAL_STATE = Memory.ReadU32(Main.scriptContext + Offsets.SCRIPT_CONTEXT_GLOBAL_STATE);
-
-        if (!GLOBAL_STATE)
-            return 0;
+        if (!GLOBAL_STATE) return 0;
 
         const GT = Lua.gt(GLOBAL_STATE);
-
-        if (!GT)
-            return 0;
+        if (!GT) return 0;
 
         Main.inverseCache = Main.Inverse(BigInt(Memory.ReadU32(GT + Offsets.GLOBAL_STATE_CKEY) + (GT + Offsets.GLOBAL_STATE_CKEY)) & 0xFFFFFFFFn, 1n << 32n) & 0xFFFFFFFFn;
         Main.executorGlobalState = Lua.newthread(GLOBAL_STATE);
 
-        if (!Main.executorGlobalState)
-            return 0;
+        if (!Main.executorGlobalState) return 0;
 
         Lua.LockObject(Main.executorGlobalState);
         Lua.SetThreadIdentityAndSandbox(Main.executorGlobalState, 7);
         Lua.setfield(GLOBAL_STATE, Lua.REGISTRYINDEX, "_GLOBAL_STATE_DO_NOT_REMOVE_");
         
         Custom.InstallFunctions(Main.executorGlobalState);
-
         return Main.executorGlobalState;
     },
     
     CreateProto(L, protoData) {
         const proto = Lua.alloc(L, 76);
-        
         Lua.link(L, proto, Lua.type.PROTO);
         
         const constants = Lua.alloc(L, protoData.constants.length * 16);
@@ -119,34 +97,28 @@ let Main = {
         protoData.protos.forEach((v, k) => Memory.WriteU32(protos + k * 4, Main.CreateProto(L, v)));
         protoData.upvalueNames.forEach((v, k) => Memory.WriteU32(upvalues + k * 4, Lua.newlstr(L, v)));
         
-        // tag for checkclosure
         Memory.WriteU32(code + protoData.instructions.length * 4, 0xFFFFFFFF);
 
         for (let i = 0; i < protoData.constants.length; i++) {
-            switch (protoData.constants[i][0]) {
-            case 0: // nil
-                Memory.WriteU64(constants + i * 16, 0n);
-                Memory.WriteU32(constants + i * 16 + 8, Lua.type.NIL);
-
-                break;
-            case 1: // boolean
-                Memory.WriteU32(constants + i * 16, protoData.constants[i][1] + 0);
-                Memory.WriteU32(constants + i * 16 + 8, Lua.type.BOOLEAN);
-
-                break;
-            case 3: // number
-                for (let j = 0; j < 8; j++) {
-                    // doubles stored as 8-char string, no encryption needed?
-                    Memory.WriteU8(constants + i * 16 + j, protoData.constants[i][1].charCodeAt(j));
-                }
-                Memory.WriteU32(constants + i * 16 + 8, Lua.type.NUMBER);
-
-                break;
-            case 4: // string
-                Memory.WriteU32(constants + i * 16, Lua.newlstr(L, protoData.constants[i][1]));
-                Memory.WriteU32(constants + i * 16 + 8, Lua.type.STRING);
-
-                break;
+            let data = protoData.constants[i];
+            let addr = constants + i * 16;
+            switch (data[0]) {
+                case 0: // nil
+                    Memory.WriteU64(addr, 0n);
+                    Memory.WriteU32(addr + 8, Lua.type.NIL);
+                    break;
+                case 1: // boolean
+                    Memory.WriteU32(addr, data[1] + 0);
+                    Memory.WriteU32(addr + 8, Lua.type.BOOLEAN);
+                    break;
+                case 3: // number
+                    for (let j = 0; j < 8; j++) Memory.WriteU8(addr + j, data[1].charCodeAt(j));
+                    Memory.WriteU32(addr + 8, Lua.type.NUMBER);
+                    break;
+                case 4: // string
+                    Memory.WriteU32(addr, Lua.newlstr(L, data[1]));
+                    Memory.WriteU32(addr + 8, Lua.type.STRING);
+                    break;
             }
         }
 
@@ -174,60 +146,38 @@ let Main = {
         Memory.WriteU32(proto + Offsets.PROTO_LOCVARS, locvars - (proto + Offsets.PROTO_LOCVARS));
         Memory.WriteU32(proto + Offsets.PROTO_SOURCE, source - (proto + Offsets.PROTO_SOURCE));
 
-        // unused
-        Memory.WriteU32(proto + Offsets.PROTO_LINEDEFINED, 0);
-        Memory.WriteU32(proto + Offsets.PROTO_LASTLINEDEFINED, 0);
-        Memory.WriteU32(proto + Offsets.PROTO_GCLIST, 0);
-
         return proto;
     },
 
     CreateLClosure(L, proto) {
         const lcl = Lua.alloc(L, 0x14);
-
         Lua.link(L, lcl, Lua.type.FUNCTION);
-
         Memory.WriteU8(lcl + Offsets.CLOSURE_IS_C, 0);
         Memory.WriteU8(lcl + Offsets.CLOSURE_NUPVALUES, 0);
         Memory.WriteU32(lcl + Offsets.CLOSURE_GCLIST, 0);
         Memory.WriteU32(lcl + Offsets.CLOSURE_ENV, Memory.ReadU32(Lua.index2adr(L, Lua.GLOBALSINDEX)));
         Memory.WriteU32(lcl + Offsets.LCLOSURE_P, proto - (lcl + Offsets.LCLOSURE_P));
-
         return lcl;
     },
 
     ExecuteScript(bytecodeData) {
         const state = Main.InitOrGetExploitState();
-
-        if (!state) {
-            alert("Main.ExecuteScript: unable to create exploit state");
-
-            return;
-        }
+        if (!state) return console.error("Main.ExecuteScript: Fail exploit state");
 
         Main.EncodeInstructions(bytecodeData.main);
-
         const L = Lua.newthread(state);
-
         Lua.SetThreadIdentityAndSandbox(L, 7);
 
-        // stk[-1] = Instance.new("LocalScript")
-        // we don't know state of env, this is safer
         Lua.pushcfunction(L, Lua.internal.FindFunctionIndex(Main.INSTANCE_NEW));
         Lua.pushstring(L, "LocalScript");
         Lua.pcall(L, 1, 1);
         
         const instance = Memory.ReadU32(Lua.topointer(L, -1) + Offsets.UDATA_DATA_BEGIN);
-
-        // substr(1) to get rid of = at start
         Instance.SetName(instance, bytecodeData.main.source.substr(1));
 
-        // script = stk[-1]
         Lua.setglobal(L, "script");
-
         Lua.settop(L, 0);
 
-        // spawn(CreatedFunction)
         Lua.pushcfunction(L, Lua.internal.FindFunctionIndex(Main.SPAWN));
         Lua.WriteAndIncrementTop(L, Main.CreateLClosure(L, Main.CreateProto(L, bytecodeData.main)), Lua.type.FUNCTION);
         Lua.pcall(L, 1, 0, 0);
@@ -238,96 +188,47 @@ let Main = {
 
     WriteError(err) {
         const state = Main.InitOrGetExploitState();
-
-        if (!state) {
-            alert("Main.WriteError: unable to create exploit state");
-
-            return;
-        }
-
-        const L = state;
-
-        Lua.getglobal(L, "warn");
-        Lua.pushstring(L, err);
-        Lua.pcall(L, 1, 0, 0);
+        if (!state) return;
+        Lua.getglobal(state, "warn");
+        Lua.pushstring(state, err);
+        Lua.pcall(state, 1, 0, 0);
     }
 };
 
+// --- Listeners ---
 
 window.addEventListener("compileError", ({ detail }) => {
-    if (!Main.FindOrGetScriptContext()) {
-        alert("CompileError: Couldn't find ScriptContext");
-
-        return;
-    }
-
-    if (typeof MainLoop == "undefined") {
-        alert("CompileError: Couldn't find main loop");
-
-        return;
-    }
-    
-    MainLoop.queue.push({
-        name: "SendError",
-        func: Main.WriteError,
-        arg: detail.error
-    });
+    if (!Main.FindOrGetScriptContext() || typeof MainLoop == "undefined") return;
+    MainLoop.queue.push({ name: "SendError", func: Main.WriteError, arg: detail.error });
 });
 
 window.addEventListener("execute", ({ detail }) => {
-    if (!Main.FindOrGetScriptContext()) {
-        alert("Execute: Couldn't find ScriptContext");
-
+    console.log("[Main] Execution triggered");
+    if (!Main.FindOrGetScriptContext() || typeof MainLoop == "undefined") {
+        console.error("MainLoop or ScriptContext missing.");
         return;
     }
-
-    if (typeof MainLoop == "undefined") {
-        alert("Execute: Couldn't find main loop");
-
-        return;
-    }
-    
-    // I don't call InitOrGetExploitState here as I'd rather do it in main thread
 
     let [ result, err ] = Bytecode.Parse(detail.bytecode);
-
     if (err) {
-        MainLoop.queue.push({
-            name: "SendError",
-            func: Main.WriteError,
-            arg: err
-        });
-
+        MainLoop.queue.push({ name: "SendError", func: Main.WriteError, arg: err });
         return;
     }
 
     result.main.source = `${detail.source}`;
-
-    MainLoop.queue.push({
-        name: "ExecuteScript",
-        func: Main.ExecuteScript,
-        arg: result
-    });
+    MainLoop.queue.push({ name: "ExecuteScript", func: Main.ExecuteScript, arg: result });
 });
 
-// hook early enough
-// we need this for custom functions, you can't set wasmTable indexes because they just get reverted for some odd reason...
+// --- Hooks ---
 let resumeMainLoopValue = undefined;
-
 setTimeout(() => {
+    if (typeof Module === "undefined") return;
     Object.defineProperty(Module, "resumeMainLoop", {
         get: () => resumeMainLoopValue,
         set: (v) => {
-            if (resumeMainLoopValue !== undefined) {
-                console.log("[Main] ??? set twice");
-    
-                return v;
-            }
-    
+            if (resumeMainLoopValue !== undefined) return v;
             resumeMainLoopValue = v;
-    
             Custom.Init();
-    
             return v;
         }
     });
